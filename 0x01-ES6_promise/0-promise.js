4738528d83e0cad45371aa0797a59d5e07d985ba
Isquare007 <akibrahiim2019@gmail.com>
2023-03-21 17:32:31 +0100

    solution to task 2-4 format fro 0&1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-    return new promise((resolve, reject) =>{
+    return new Promise((resolve, reject) =>{
         resolve()
     }) 
 }
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils.js';
+export default function handleProfileSignup() {
+    const photo = uploadPhoto();
+    const user = createUser();
+
+    return Promise.all([photo, user])
+        .then((values) => {
+            const profile = values.body;
+            const first = values[1].firstName;
+            const last = values[1].lastName;
+
+            console.log(`${profile} ${first} ${last}`)
+        })
+        .catch(() => console.log("Signup system offline"))
+}
