01caa2435c2ad88b77cd6a9b703aa54e90f2f75d
Isquare007 <akibrahiim2019@gmail.com>
2023-03-23 19:52:56 +0100

    3- currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // Code
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  // Name
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
