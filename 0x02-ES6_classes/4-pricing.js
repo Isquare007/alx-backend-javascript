a60bbb7e5d861afd06b12c76e2f27273f05bf22f
Isquare007 <akibrahiim2019@gmail.com>
2023-03-23 22:53:59 +0100

    task 4 &5

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Amount
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  // Currency
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} ${(this.currency.code)}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+export default Pricing;
