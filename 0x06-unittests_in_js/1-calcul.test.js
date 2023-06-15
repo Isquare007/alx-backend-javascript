8eacaa83fa3a15d3c956fdb5f7d57a073682eeee
Isquare007 <akibrahiim2019@gmail.com>
2023-06-15 15:52:20 +0100

    1-calculus

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 5.5, 3.3), 9);
+      assert.strictEqual(calculateNumber('SUM', 8.9, 2.1), 11);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return the difference of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 4.2, 10.8), 7);
+      assert.strictEqual(calculateNumber('SUBTRACT', 2.9, 6.1), 3);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return the division result of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 15.6, 4.2), 4);
+      assert.strictEqual(calculateNumber('DIVIDE', 10.5, 0), 'Error');
+    });
+  });
+});
