9ffda0050a38af6b36bb72d53f9f73020fa2ce06
Isquare007 <akibrahiim2019@gmail.com>
2023-06-16 06:11:05 +0100

    corrected indentation

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,10 +1,9 @@
 function getPaymentTokenFromAPI(success) {
-    if (success) {
-      return Promise.resolve({ data: 'Successful response from the API' });
-    } else {
-      return Promise.resolve();
-    }
+  if (success) {
+    return Promise.resolve({ data: 'Successful response from the API' });
+  } else {
+    return Promise.resolve();
   }
-  
-  module.exports = { getPaymentTokenFromAPI };
-  
+}
+
+module.exports = { getPaymentTokenFromAPI };
