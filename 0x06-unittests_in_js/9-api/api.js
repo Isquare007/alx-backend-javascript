38906b17fcdf7c576c41ae85e427914b8168a56e
Isquare007 <akibrahiim2019@gmail.com>
2023-06-16 16:37:40 +0100

    added missing columns

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,11 +5,11 @@ const port = 7865;
 
 app.get('/', (req, res) => {
   res.end('Welcome to the payment system');
-})
+});
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
   res.end(`Payment methods for cart :${req.params.id}`);
-})
+});
 
 app.listen(port, () => {
   console.log('API available on localhost port 7865');
